1df681f91644442bd791fad70561a4b7f4e458d7
nasenatalie21 <nasenatalie@gmail.com>
2019-10-11 16:16:45 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# camera-appv6
+# camera-app
+Super awesome camera web app built with HTML, CSS, and JS.
---
+++ app.js
@@ -0,0 +1,68 @@
+// Set constraints for the video stream
+var front = false;
+//var constraints = {video: {facingMode:'user'}, audio: false };
+var constraints = {video: {facingMode: front ? 'user' : 'environment'}};
+var track = null;
+
+
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger"),
+    cameraSwitch = document.querySelector("#camera--switch"),
+    saveImage = document.querySelector("#save--image");
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    //constraints.video = { facingMode: shouldFaceUser ? 'user' : 'environment'}
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+    // track.stop();
+};
+
+// Switch Camera to back/front camera
+// cameraSwitch.addEventListener('click', function(){
+//    if(stream == null) return
+//    stream.getTracks().array.forEach(t => {
+//        t.stop();
+//    });
+//    shouldFaceUser = !shouldFaceUser;
+   
+// });
+
+cameraSwitch.onclick = function(){
+    cameraView.pause();
+    cameraView.srcObject = null;
+    front = !front; 
+    cameraStart();
+};
+
+// Save the image to local gallery
+saveImage.onclick = function() {
+    var gh = cameraOutput.src;
+    var a  = document.createElement('a');
+    a.href = gh;
+    a.download = 'image.png';
+
+    a.click()
+};
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
